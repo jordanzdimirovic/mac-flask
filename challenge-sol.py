119201ad682cb07651b34d32be317f3ab30a5b1d
Jordan Zdimirovic <jordan@Jordans-MacBook-Pro.local>
2023-08-14 22:45:48 +1000

    Added say endpoint to challenge solution

---
+++ challenge-sol.py
@@ -0,0 +1,21 @@
+# Create Flask server
+from flask import Flask, jsonify, request
+
+from ttshelper import say
+
+app = Flask(__name__)
+
+# Create routes
+@app.route("/say", methods=["POST"])
+def say_endpoint():
+    # Get request json
+    to_say = request.json['message']
+    
+    # Say using tts helper
+    say(to_say)
+    
+    return f"Successfully said {to_say}"
+
+
+# Run on all IPs
+app.run("0.0.0.0", 81)
